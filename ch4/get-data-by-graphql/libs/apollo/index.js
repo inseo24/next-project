173a5bce0bd3010ffe76d21dce8a6c480b805b3c
choi seoin <inseo@uupfi.com>
2023-10-09 19:23:30 +0900

    get data by graphql

---
+++ ch4/get-data-by-graphql/libs/apollo/index.js
@@ -0,0 +1,44 @@
+import { useMemo } from "react";
+import {
+    ApolloClient,
+    HttpLink,
+    InMemoryCache
+} from '@apollo/client';
+
+let uri = '/api/graphql';
+let apolloClient;
+
+export function useApollo(initialState) {
+    return useMemo(
+        () => initApollo(initialState), [initialState]
+    );
+}
+
+function createApolloClient() {
+    return new ApolloClient({
+        ssrMode: typeof window === 'undefined',
+        link: new HttpLink({ uri }),
+        cache: new InMemoryCache(),
+    })
+}
+
+export function initApollo(initialState = null) {
+    const client = apolloClient || createApolloClient();
+
+    if (initialState) {
+        client.cache.restore({
+            ...client.extract(),
+            ...initialState
+        })
+    }
+
+    if (typeof window === "undefined") {
+        return client;
+    }
+
+    if (!apolloClient) {
+        apolloClient = client;
+    }
+
+    return client;
+}
