ece64c21ac715cc4d5acdef0b5b29e10ea55cd92
choi seoin <inseo@uupfi.com>
2023-10-15 17:07:02 +0900

    5.2 boilerplate

--- ch5-state-management/pages/_app.js
+++ ch5-state-management/pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+      </Head>
+      <Navbar />
+      <div className="w-9/12 m-auto pt-10">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- ch5-state-management/pages/index.js
+++ ch5-state-management/pages/index.js
@@ -1,8 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Counter from '../components/Counter'
+import ProductCard from '../components/ProductCard';
+import products from '../data/items';
 
-export default function Home() {
-  return <Counter initialCount={1} />
+function Home() {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </div>
+  );
 }
+
+export default Home;
